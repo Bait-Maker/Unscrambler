4a9595d0b36b7cdf587a9405fd613c1c5b38bd8d
Bait-Maker <87659421+Bait-Maker@users.noreply.github.com>
2021-11-19 00:38:42 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@
       ---------------
    3 | n | m |   |   |
       ---------------
-
  *
  */
 
@@ -26,6 +25,7 @@
 #include <vector>
 #include <fstream>
 #include <cassert>
+#include <ctime>
 using namespace std;
 
 class Board{
@@ -33,33 +33,109 @@ class Board{
 public:
     Board(int size){ boardSize = size;}
     void displayBoard();
+    void generateBoard(const vector <string> &data, int piecesLeft);
 
 private:
     int boardSize;
-    vector <string> Boards;
+    string pieces;
 
 };// end class Board
 
 void Board ::displayBoard() {
+    int counter = 0;
+
     cout << "Current board:" << endl;
     cout << "       ";
+    // printing column numbers
     for(int i = 0; i < boardSize; i++){
-       cout << i << "   ";
-        for(int j = 0; j < boardSize ; j++){
-            cout << "   " << j << " |   |";
-            if(j == boardSize-1){
-                cout << endl;
-            }
-        }
+        cout << i << "   ";
+    }
+
+    // printing dashes
+    cout << endl;
+    cout << "      ";
+    for(int x = 0; x < boardSize; x++){
+        cout << "---";
+    }
+    for (int d = 0; d < boardSize-1; d++) {
+        cout << "-";
     }
     cout << endl;
 
+    // printing board rows
+    for(int i = 0; i < boardSize; i++){
+        cout << "   " << i;
+        for(int j = 0; j < boardSize; j++){
+                cout << " | " << pieces.at(counter);
+                counter++;
+        }
 
+        cout << " |" << endl;
+        cout << "      ";
 
+        // printing dashes
+        for(int x = 0; x < boardSize; x++){
+            cout << "---";
+        }
+        for (int d = 0; d < boardSize-1; d++) {
+            cout << "-";
+        }
+        cout << endl;
 
+    }
 
 }// end displayBoard()
 //----------------------------------------------------------------------------
+
+// adds random words onto the board until the board is full
+void Board ::generateBoard(const vector <string> &data, int piecesLeft){
+
+    string temp, word;
+
+    while(piecesLeft >= 3){
+        // not appending " " to random words
+        // when 4 spaces remain
+        if(piecesLeft == 4){
+            word = data.at(rand()%data.size());
+            if (word.size() > piecesLeft){
+                continue;
+            }
+            else {
+                temp.append(word);
+                piecesLeft = piecesLeft - word.size();
+//                cout << piecesLeft << endl;
+            }
+        }
+        word = data.at(rand()%data.size());
+        word.append(" ");
+        if(word.size() > piecesLeft){
+            continue;
+        }
+        else{
+            temp.append(word);
+            piecesLeft = piecesLeft - word.size();
+            cout << piecesLeft << endl;
+
+            }
+        }// end while
+
+    if(piecesLeft <=2){
+        while(piecesLeft != 0){
+            temp.append(" ");
+            piecesLeft--;
+        }
+
+    }// end while
+
+    pieces = temp; // setting pieces equal to
+
+
+    // making sure it was generating the correct random words
+//    for(int x = 0; x < temp.size(); x++){
+//        cout << temp.at(x);
+//    }
+//    cout << endl;
+}
 
 void readInDictionary(vector <string> & dictionary){
 
@@ -80,31 +156,82 @@ void readInDictionary(vector <string> & dictionary){
 //----------------------------------------------------------------------------
 
 
+
+
 int main(){
 
+    srand(1);
     char userInput;
+    int sizeInput;
+    int totalPiecesLeft;
     vector <string> dictionary;
-    Board myBoard(4);
-
-    readInDictionary(dictionary);
-
-    myBoard.displayBoard();
 
 
+    readInDictionary(dictionary); // reads in words from dictionary and stores them into vector
+
+
+
+
+    // displaying start up text
     cout << "Program 5: Unscrambler\n"
          << "CS 141, Fall 2021, UIC\n" << endl;
 
-    cout << "You will be presented with a board that is filled with scrambled words. Rotate the rows and columns\n"
+    cout << "You will be presented with a board that is filled\n"
+         << "with scrambled words. Rotate the rows and columns\n"
          << "to get the words displayed in the right order.\n" << endl;
 
-    cout << "13893 words have been read in from the dictionary.\n" << endl;
+    cout << dictionary.size() << " words have been read in from the dictionary." << endl;
+    cout << endl;
 
     cout << "Enter Q to quit or B to begin:";
     cin >> userInput;
     userInput = tolower(userInput);
 
     if(userInput == 'q'){
-        cout << "Thanks for playing!" << endl;
+        cout << "Thank you for playing!" << endl;
+    }
+    else {
+        cout << "Choose your board size (must be a number greater than or equal to 4):";
+        cin >> sizeInput;
+        cout << endl;
+
+
+        while(sizeInput < 4){
+            cout << "The board size must be at least 4. Retry.";
+            cout << "Choose your board size (must be a number greater than or equal to 4):";
+            cin >> sizeInput;
+        }
+        Board myBoard(sizeInput);                  // setting board size
+        totalPiecesLeft = sizeInput * sizeInput;   // initializing totalPieces
+        //TODO generate board
+        myBoard.generateBoard(dictionary, totalPiecesLeft);
+        myBoard.displayBoard();
+
+        cout << "Choose the number of times you would like the board to be scrambled (must be a single number >= 1):";
+        cin >> sizeInput;
+
+        // making sure the number of scrambles is at least 1
+        while(sizeInput < 1){
+            cout << "The number of times you select must be at least 1. Retry.\n"
+                 << "Choose the number of times you would like the board to be scrambled (must be a single number >= 1):";
+            cin >> sizeInput;
+        }
+
+
+
+
+        // !!appending space works so far!!
+//        string test = "Test";
+//        test.append(" ");
+//        cout << test << "space" << endl;
+//        cout << endl;
+
+        myBoard.displayBoard();
+
+
+
+
+
     }
 
 
